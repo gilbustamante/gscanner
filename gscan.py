2e7fe71d26486ef92f98ebab38dbe1f8371d38ad
Gil Bustamante <65842842+gilbustamante@users.noreply.github.com>
2020-09-22 19:32:08 -0700

    Add instructions and try/except block

--- gscan.py
+++ gscan.py
@@ -9,6 +9,8 @@ WRITE_KEY = 'v'
 
 SCAN_RUNNING = False
 
+print('Press F7 to start, F8 to stop.')
+
 def start_scanning():
     global SCAN_RUNNING
     SCAN_RUNNING = True
@@ -23,10 +25,14 @@ def on_press(key):
     # Change 'f7' to whichever key you want to start
     if key == keyboard.Key.f7 and not SCAN_RUNNING:
         t = threading.Thread(target=start_scanning)
+        print('GScan running...')
         t.start()
     # Change 'f8' to whichever key you want to stop
     elif key == keyboard.Key.f8:
         SCAN_RUNNING = False
         
-with keyboard.Listener(on_press=on_press) as listener:
-    listener.join()
+try:
+    with keyboard.Listener(on_press=on_press) as listener:
+        listener.join()
+except KeyboardInterrupt:
+    print('Closing GScan...')
