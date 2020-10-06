db639b58be304646c67a0592f15def5433550557
Gil Bustamante <65842842+gilbustamante@users.noreply.github.com>
2020-10-05 19:36:48 -0700

    Refactor functions

--- gscan.py
+++ gscan.py
@@ -5,18 +5,15 @@ import time
 import sys
 import platform
 
-# Edit these two to change key/frequency
-SLEEP_TIME = 3
-WRITE_KEY = 'v'
-
-SCAN_RUNNING = False
-
-if platform.system() == 'Windows':
-    print('Press F7 to start, F8 to pause, CTRL+PAUSE/BREAK to quit.')
-else:
-    print('Press F7 to start, F8 to stop, CTRL+C to quit.')
+def determine_os():
+    """If the user is using Windows, display appropriate instructions"""
+    if platform.system() == 'Windows':
+        print('Press F7 to start, F8 to pause, CTRL+PAUSE/BREAK to quit.')
+    else:
+        print('Press F7 to start, F8 to pause, CTRL+C to quit.')
 
 def start_scanning():
+    """Press the WRITE_KEY every SLEEP_TIME seconds"""
     global SCAN_RUNNING
     SCAN_RUNNING = True
 
@@ -29,6 +26,7 @@ def start_scanning():
         time.sleep(SLEEP_TIME)
 
 def on_press(key):
+    """Check which key is being pressed and either start or pause"""
     global SCAN_RUNNING
     # Change 'f7' to whichever key you want to start
     if key == keyboard.Key.f7 and not SCAN_RUNNING:
@@ -40,9 +38,20 @@ def on_press(key):
         print('GScan paused')
         SCAN_RUNNING = False
         
-try:
-    with keyboard.Listener(on_press=on_press) as listener:
-        listener.join()
-except KeyboardInterrupt:
-    print('Closing GScan...')
-    sys.exit()
+if __name__ == '__main__':
+    # Find OS and display instructions
+    determine_os()
+
+    # Edit these to change key/frequency
+    SLEEP_TIME = 3
+    WRITE_KEY = 'v'
+
+    # Start program paused
+    SCAN_RUNNING = False
+
+    try:
+        with keyboard.Listener(on_press=on_press) as listener:
+            listener.join()
+    except KeyboardInterrupt:
+        print('Closing GScan...')
+        sys.exit()
